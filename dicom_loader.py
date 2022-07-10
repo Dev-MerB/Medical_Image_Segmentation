7c5ac5dff5e58afa1707864fc3b1200c24844bdb
Jintopia <matrosica0808@gmail.com>
2022-07-10 14:59:56 +0000

    update code

--- dicom_loader.py
+++ dicom_loader.py
@@ -2,39 +2,46 @@ import os
 import numpy as np
 import torch
 from torch.utils import data
-from torchvision import transforms as T
-from torchvision.transforms import functional as F
 from PIL import Image
 import natsort
 from pydicom import dcmread
 import nibabel as nib
+import glob
 import cv2
-import matplotlib.pyplot as plt
-# np.set_printoptions(threshold = np.inf, linewidth = np.inf)
-class ImageFolder(data.Dataset):
-	def __init__(self, root,mode='train',augmentation_prob=0.4, model='U_Net'):
-		"""Initializes image paths and preprocessing module."""
-		self.root = root
-		
-		# GT : Ground Truth
-		self.model = model
-		if model == 'MIA':
-			self.GT_paths = root[:-1]+'_MIA_GT/'
-		else:
-			self.GT_paths = root[:-1]+'_GT/'
-		self.image_paths = list(natsort.natsorted(os.listdir(root)))
-		self.mode = mode
-		self.augmentation_prob = augmentation_prob
-		print("image count in {} path :{}".format(self.mode,len(self.image_paths)))
 
-	def __getitem__(self, index):
-		image_path = self.image_paths[index]
-		image_GT_path = image_path[:8]+'_'+image_path[8:12]+'.nii'
-		GT_path = self.GT_paths + image_GT_path
+class LungCancerDataset(data.Dataset):
+    def __init__(self, root_path, size, mode="train"):
+        super(LungCancerDataset, self).__init__()
 
-		ds = dcmread(self.root+image_path)
-		image = (ds.pixel_array/4095).astype(np.float32)
+        self.root_path = root_path
+        self.mode = mode
+        self.transforms = None
+        self.dcm = None
+        self.nifti = None
 
+        if (self.mode == "train"):
+            train_dir = os.path.join(self.root_path, "train")
+            self.dcm = natsort.natsorted(glob.glob(os.path.join(train_dir,"*.dcm")))
+            self.nifti = natsort.natsorted(glob.glob(os.path.join(train_dir,"*.nii")))
+        elif (self.mode == "valid"):
+            val_dir = os.path.join(self.root_path, "valid")
+            self.dcm = natsort.natsorted(glob.glob(os.path.join(val_dir,"*.dcm")))
+            self.nifti = natsort.natsorted(glob.glob(os.path.join(val_dir,"*.nii")))
+        elif self.mode == "test":
+            test_dir = os.path.join(self.root_path,"test")
+            self.dcm = natsort.natsorted(glob.glob(os.path.join(test_dir,"*.dcm")))
+            self.nifti = natsort.natsorted(glob.glob(os.path.join(test_dir,"*.nii")))
+        else:
+            raise NotImplementedError
+
+    def __len__(self):
+            return len(self.dcm)
+
+    def __getitem__(self, idx):
+        ds = dcmread(self.dcm[idx])
+        dcm = (ds.pixel_array/4095).astype(np.float32)
+
+		### display range apply 
 		# center = ds[0x0028, 0x1050].value
 		# width = ds[0x0028, 0x1051].value
 		# minRange = int(center - round(width/2))
@@ -49,55 +56,16 @@ class ImageFolder(data.Dataset):
 
 		# im = np.clip(im , np.min(im)+(crit+minRange),np.min(im)+(crit+maxRange))
 		# image = (im/4095).astype(np.float32)
-		# fig = plt.figure()
-		# ax1 = fig.add_subplot(1,2,1)
-		# ax1.imshow(image,cmap='gray')
-		
-		image= torch.from_numpy(image).contiguous().type(torch.FloatTensor)
-		
-		image = image.unsqueeze(0)
+		###
+
+        dcm= torch.from_numpy(dcm).contiguous().type(torch.FloatTensor)
+        dcm = dcm.unsqueeze(0)
 		# print(image.shape)
 
+        nifti = nib.load(self.nifti[idx])
+        gt = nifti.get_fdata()
+        gt = torch.from_numpy(gt.transpose((2,1,0))).type(torch.FloatTensor)
 
-		gt = nib.load(GT_path)
-		gt = gt.get_fdata()
-		# ax2 = fig.add_subplot(1,2,2)
-		# ax2.imshow(gt.transpose((1,0,2)),cmap='gray')
-		# plt.show()
-		gt = torch.from_numpy(gt.transpose((2,1,0))).type(torch.FloatTensor)
-		# print("gt :", gt.shape)
-		return image, gt, image_path
+        sample = {"dcm": dcm, "nifti": gt, "affine":nifti.affine}
 
-	def __len__(self):
-		"""Returns the total number of font files."""
-		return len(self.root+str(self.image_paths))
-
-def get_dicom_loader(image_path, batch_size, num_workers=2, mode='train',augmentation_prob=0.4,model='U-Net'):
-	"""Builds and returns Dataloader."""
-
-	data_image = ImageFolder(root = image_path, mode=mode,augmentation_prob=augmentation_prob,model = model)
-	#test_index = []
-
-	# index = 0
-	# for path in dataset.image_paths:
-	# 	split_path = path.split("_")
-	# 	if int(split_path[0]) in test_idx:
-	# 		test_index.append(index)
-
-	# 	index = index + 1
-
-	# total_index = list(range(0, len(dataset.image_paths)))
-
-
-	# train_idx = [index for index in total_index if index not in test_index]
-
-	# dataset_train = torch.utils.data.Subset(dataset, train_idx)
-	# dataset_test = torch.utils.data.Subset(dataset, test_index)
-
-	dataset = torch.utils.data.Subset(data_image, list(range(0, len(data_image.image_paths))))
-	data_loader = data.DataLoader(dataset=dataset,
-								  batch_size=batch_size,
-								  shuffle=True,
-								  num_workers=num_workers)
-
-	return data_loader
+        return sample
--- solver.py
+++ solver.py
@@ -1,55 +1,34 @@
 import os
 import numpy as np
 import torch
-import torchvision
 from torch import optim
 import torch.nn.functional as F
-from skimage.transform import resize
-from network import U_Net,R2U_Net,AttU_Net,R2AttU_Net,Nested_UNet
-import csv
+from network import U_Net
 from PIL import Image
 import cv2
-from vol_eval import *
 from tqdm import tqdm
-from pytorchtools import EarlyStopping
 import nibabel as nib
-import matplotlib.pyplot as plt
+
 class DiceLoss(torch.nn.Module):
     def __init__(self, weight=None):
         super(DiceLoss, self).__init__()
 
     def forward(self, gt, predict, smooth=1e-5):
-
-        # gt = one_hot encoding
-        # predict = network output
         intersection = (gt * predict).sum()
         dice_coefficient = (2*abs(intersection) + smooth) / (abs(gt.sum()) + abs(predict.sum()) + smooth)
-        flat_gt = gt.view(gt.size(0),-1)
-        flat_pred = predict.view(predict.size(0),-1)
 
         loss_dice = (1-dice_coefficient)
-        loss_func =  torch.nn.BCELoss()
-        loss_bce = loss_func(flat_pred,flat_gt)
-        loss_total = 0.5*loss_bce+loss_dice
-        # loss_total = torch.log((torch.exp(loss_dice) + torch.exp(-loss_dice)) / 2.0)
 
-        return loss_total
+        return loss_dice
 
 class Solver(object):
-	def __init__(self, config, train_loader, valid_loader, test_loader):
-
-		# Data loader
-		self.train_loader = train_loader
-		self.valid_loader = valid_loader
-		self.test_loader = test_loader
-
+	def __init__(self, config):
 		# Models
-		self.unet = None
+		self.model = None
 		self.optimizer = None
 		self.img_ch = config.img_ch
 		self.output_ch = config.output_ch
 		self.criterion = torch.nn.BCELoss()
-		self.augmentation_prob = config.augmentation_prob
 
 		# Hyper-parameters
 		self.lr = config.lr
@@ -57,13 +36,7 @@ class Solver(object):
 		self.beta2 = config.beta2
 
 		# Training settings
-		self.num_epochs = config.num_epochs
-		self.num_epochs_decay = config.num_epochs_decay
-		self.batch_size = config.batch_size
-
-		# Step size
-		self.log_step = config.log_step
-		self.val_step = config.val_step
+		self.num_epochs = config.num_epoch
 
 		# Path
 		self.model_path = config.model_path
@@ -80,197 +53,78 @@ class Solver(object):
 		self.build_model()
 
 	def build_model(self):
-		"""Build generator and discriminator."""
-		if self.model_type =='U_Net':
-			self.unet = U_Net(img_ch=self.img_ch,output_ch=1)
-		elif self.model_type =='R2U_Net':
-			self.unet = R2U_Net(img_ch=self.img_ch,output_ch=1,t=self.t)
-		elif self.model_type =='AttU_Net':
-			self.unet = AttU_Net(img_ch=self.img_ch,output_ch=1)
-		elif self.model_type == 'R2AttU_Net':
-			self.unet = R2AttU_Net(img_ch=self.img_ch,output_ch=1,t=self.t)
-		elif self.model_type == 'NestedU_Net':
-			self.unet = Nested_UNet(img_ch=self.img_ch,output_ch=1)
-
-		self.optimizer = optim.Adam(list(self.unet.parameters()),
+		self.model = U_Net(img_ch=self.img_ch,output_ch=1)
+		self.optimizer = optim.Adam(list(self.model.parameters()),
 									  self.lr, [self.beta1, self.beta2],eps =1e-8)
-		self.unet.to(self.device)
+		self.model.to(self.device)
 
 		# self.print_network(self.unet, self.model_type)
 
-	def print_network(self, model, name):
-		"""Print out the network information."""
-		num_params = 0
-		for p in model.parameters():
-			num_params += p.numel()
-		print(model)
-		print(name)
-		print("The number of parameters: {}".format(num_params))
-
-	def to_data(self, x):
-		"""Convert variable to tensor."""
-		if torch.cuda.is_available():
-			x = x.cpu()
-		return x.data
-
-	def update_lr(self, g_lr, d_lr):
-		for param_group in self.optimizer.param_groups:
-			param_group['lr'] = self.lr
-
-	def reset_grad(self):
-		"""Zero the gradient buffers."""
-		self.unet.zero_grad()
-
-	def compute_accuracy(self,SR,GT):
-		SR_flat = SR.view(-1)
-		GT_flat = GT.view(-1)
-
-		acc = GT_flat.data.cpu()==(SR_flat.data.cpu()>0.5)
-
-	def tensor2img(self,x):
-		img = (x[:,0,:,:]>x[:,1,:,:]).float()
-		img = img*255
-		return img
-
-
-	def train(self):
-		"""Train encoder, generator and discriminator."""
+	def train(self,train_loader, valid_loader):
 
 		#====================================== Training ===========================================#
 		#===========================================================================================#
+		lr = self.lr
+		min_loss = 99999
+		for epoch in range(self.num_epochs):
+			self.model.train(True)
+			train_losses = []
+			valid_losses = []
+			for i, data in enumerate(tqdm(train_loader)):
+				dcm = data["dcm"].to(self.device)
+				gt = data["nifti"].to(self.device)
+
+				pred= self.model(dcm)
+				pred = torch.sigmoid(pred)
+				self.criterion=DiceLoss() #BCE not use
+				loss = self.criterion(gt,pred)
+				train_losses.append(loss.item())
+
+				self.optimizer.zero_grad()
+				loss.backward()
+				self.optimizer.step()
+
+			self.model.train(False)
+			self.model.eval()
+		#====================================== validation ===========================================#
+		#===========================================================================================#
+			for i, data in enumerate(tqdm(valid_loader)):
+				dcm = data["dcm"].to(self.device)
+				gt = data["nifti"].to(self.device)
+
+				pred= self.model(dcm)
+				pred = torch.sigmoid(pred)
+				self.criterion=DiceLoss() #BCE not use
+				loss = self.criterion(gt,pred)
+				valid_losses.append(loss.item())
+				
+			# Print the log info
+			train_loss = np.average(train_losses)
+			valid_loss = np.average(valid_losses)
+			print('Epoch [%d/%d], Train Loss: %.4f,  Valid Loss: %.4f, LR:%f' % 
+								(epoch+1, self.num_epochs, train_loss, valid_loss,lr))
+			unet_path = os.path.join(self.model_path,"0215_EV3", '%s_%d_%f.pkl' % (self.model_type,epoch+1,lr))
+
+			if min_loss > valid_loss : 
+				min_loss = valid_loss
+				torch.save(self.unet.state_dict(), unet_path)
+
+	def test(self, test_loader):
 		
-		#unet_path = os.path.join(self.model_path, '%s-%d-%.4f-%d-%.4f.pkl' %(self.model_type,self.num_epochs,self.lr,self.num_epochs_decay,self.augmentation_prob))
-		unet_path = os.path.join(self.model_path, 's.pkl')
-		early_stopping = EarlyStopping(patience=250)
-		train_losses = []
-		valid_losses = []
-		# U-Net Train
-		if os.path.isfile(unet_path):
-			# Load the pretrained Encoder
-			self.unet.load_state_dict(torch.load(unet_path))
-			print('%s is Successfully Loaded from %s'%(self.model_type,unet_path))
-		else:
-			# Train for Encoder
-			lr = self.lr
-			min_loss = 99999
-			for epoch in range(self.num_epochs):
+		self.model.load_state_dict(torch.load(model_path))
+		self.model.train(False)
+		self.model.eval()
 
-				self.unet.train(True)
-				epoch_loss = 0
-				for i, (images, GT,empty) in enumerate(tqdm(self.train_loader)):
-					# GT : Ground Truth
-
-					images = images.to(self.device)
-					GT = GT.to(self.device)
-					
-					# SR : Segmentation Result
-					SR = self.unet(images)
-					SR_probs = torch.sigmoid(SR)
-
-					# SR_flat = SR_probs.view(SR_probs.size(0),-1)
-
-					# GT_flat = GT.view(GT.size(0),-1)
-					# loss =self.criterion(SR_flat,GT_flat)
-
-					self.criterion=DiceLoss() #BCE not use
-					loss = self.criterion(GT,SR_probs)
-					train_losses.append(loss.item())
-					# Backprop + optimize
-					self.reset_grad()
-					loss.backward()
-					self.optimizer.step()
-
-				if(epoch+1) >(self.num_epochs- self.num_epochs_decay):
-					lr -= (self.lr/float(self.num_epochs_decay))
-					for param_group in self.optimizer.param_groups:
-						param_group['lr']=lr
-					print('Decay learning rate to lr :{}'.format(lr))
-				######################
-				# Validate the model #
-				######################
-				self.unet.train(False)
-				self.unet.eval()
-				for i, (images,GT,empty) in enumerate(tqdm(self.valid_loader)):
-					images = images.to(self.device)
-					GT = GT.to(self.device)
-					SR = torch.sigmoid(self.unet(images))
-
-					# SR_flat = SR.view(SR.size(0),-1)
-
-					# GT_flat = GT.view(GT.size(0),-1)
-					# loss = self.criterion(SR_flat,GT_flat)
-					loss = self.criterion(GT,SR)
-					epoch_loss += loss.item()
-
-					valid_losses.append(loss.item())
-
-				# Print the log info
-				train_loss = np.average(train_losses)
-				valid_loss = np.average(valid_losses)
-				valid_losses = []
-				train_losses = []
-				print('Epoch [%d/%d], Train Loss: %.4f,  Valid Loss: %.4f, LR:%f' % 
-									(epoch+1, self.num_epochs, train_loss, valid_loss,lr))
-				
-				early_stopping(valid_loss, self.unet)
-				
-				unet_path = os.path.join(self.model_path,"0215_EV3", '%s_%d_%f.pkl' % (self.model_type,epoch+1,lr))
-
-				if min_loss > epoch_loss : 
-					min_loss = epoch_loss
-					torch.save(self.unet.state_dict(), unet_path)
-
-				if early_stopping.early_stop:
-					print("Stopped ", epoch+1)
-					torch.save(self.unet.state_dict(), unet_path)
-					break
-
-	def test(self):
-
-		del self.unet
-		self.build_model()
-		# unet_path = os.path.join(self.model_path, '%s-%d-%d.pkl' % (self.model_type, self.fold, epoch))
-		unet_path = os.path.join(self.model_path,'%s.pkl' % (self.test_model))
-		print(unet_path)
-		self.unet.load_state_dict(torch.load(unet_path))
-
-		self.unet.train(False)
-		self.unet.eval()
-
-		for i, (images, GT,image_path) in enumerate(tqdm(self.test_loader)):
-			images= images.to(self.device)
-			image_path = list(image_path)
-			gt = GT[0][0].cpu().numpy()
-			gt = gt.transpose((1,0)).astype(np.uint16)
+		for i, data in enumerate(tqdm(test_loader)):
+			dcm = data["dcm"].to(self.device)
+			gt = data["nifti"]
+			affine = data["affine"]
+			pred = torch.sigmoid(self.model(dcm))
+			pred = (pred>0.5).float()
+			pred0 = pred.unsqueeze(0)
+			pred0 = pred0.unsqueeze(0)
 			
-
-			SR = torch.sigmoid(self.unet(images))
-			SR = (SR>0.5).float()
-			SR = SR[0][0].cpu().numpy()
-			SR = SR.transpose((1,0)).astype(np.uint16)
-
-			
-			pred = nib.Nifti1Image(SR,affine=np.eye(4))
-			gt_o = nib.Nifti1Image(gt,affine=np.eye(4))
-			
-			# fig = plt.figure()		
-			# ax1 = fig.add_subplot(1,3,1)
-			# ax1.imshow(SR,cmap='gray')
-			# ax2 = fig.add_subplot(1,3,2)
-			# ax2.imshow(images.cpu().numpy().squeeze(), cmap='gray')
-			# ax3 = fig.add_subplot(1,3,3)
-			# ax3.imshow(gt,cmap='gray')
-			# plt.show()
-
-			fx = 320 / 256
-			fy = 280 / 256
-			rescale = resize(SR,(320,280,1))
-			rescale = nib.Nifti1Image(rescale,affine=np.eye(4))
-
-			nib.save(gt_o, os.path.join(self.result_path,"GT",image_path[0].split(".")[0]+".nii"))
+			gt = nib.Nifti1Image(gt,affine=affine)
+			pred = nib.Nifti1Image(pred0.numpy(),affine=affine)
+			nib.save(gt, os.path.join(self.result_path,"GT",image_path[0].split(".")[0]+".nii"))
 			nib.save(pred, os.path.join(self.result_path,"Predict",image_path[0].split(".")[0]+".nii"))
-			nib.save(rescale, os.path.join(self.result_path,"Rescale",image_path[0].split(".")[0]+".nii"))
-
-
-		eval_volume_from_mask( os.path.join(self.result_path,"GT"), os.path.join(self.result_path,"Predict"))
-		
